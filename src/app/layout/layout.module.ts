6182146c8d4722dc4699cf0e43c657b2812fc95b
Pavel Baštinec <pavel4628@email.cz>
2023-08-17 01:41:02 +0200

    feature: toolbar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatRippleModule} from "@angular/material/core";
+import {MatButtonModule} from "@angular/material/button";
 
 
 
@@ -18,9 +20,12 @@ import {MatToolbarModule} from "@angular/material/toolbar";
   exports: [
     LayoutComponent
   ],
-    imports: [
-        CommonModule,
-        MatToolbarModule
-    ]
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatRippleModule,
+    NgOptimizedImage,
+    MatButtonModule
+  ]
 })
 export class LayoutModule { }

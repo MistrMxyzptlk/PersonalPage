62817e2072a0f4413ed87cd6b4757ebdfa330bb9
Pavel Baštinec <pavel4628@email.cz>
2023-10-11 15:37:26 +0200

    feature: deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+printf "building packages: \n"
+ng build
+
+printf "clearing directory: \n"
+ssh -i ~/.ssh/pipeline_rsa pipeline@mixspitlick.tech 'rm -rf ./public/*'
+
+printf "deploying packages\n"
+cd ./dist/personal-page && tar -cvzf ../build.tar.gz  . && cd ../../
+scp -i ~/.ssh/pipeline_rsa ./dist/build.tar.gz pipeline@mixspitlick.tech:
+ssh -i ~/.ssh/pipeline_rsa pipeline@mixspitlick.tech 'tar -xvf ./build.tar.gz -C ./public && rm ./build.tar.gz'
+
+printf 'build complete!\n'
+rm -rf ./dist
